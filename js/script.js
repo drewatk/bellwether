de91cb02bab2be16b3fd5b5c4caba9499cfa7f47
vritant <vritoku@gmail.com>
2017-04-16 11:38:56 -0400

    sidebar style more robust

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
+var width;
+
+$(document).ready(function() {
+   //Set height of the sidebar fixed on load
+   var width = $(window).width();
+   $('#left').css('height', $(window).height() + 'px');
+});
+
 // change active on navbar on click
 $('.nav li').click(function(){
-    $('.nav li').removeClass('active');
-    $(this).addClass('active');
-})
+  $('.nav li').removeClass('active');
+  $(this).addClass('active');
+});
+
+//prevents sidebar from resizing if just height is changed
+window.onresize = function() {
+  if($(this).width() == width) {
+    return;
+  }
+  width = $(this).width();
+  if(width <= 800) {
+    console.log("here");
+    $('#left').css('height', 'auto');
+  }
+  if(width > 800) {
+    $('#left').css('height', $(window).height() + 'px');
+  }
+};
